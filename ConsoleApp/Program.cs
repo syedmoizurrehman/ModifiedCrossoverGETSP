8aa3a2c4b8c87921bc7959f11f372429fd6141d0
Syed Moiz Ur Rehman <syedmoizurrehman@hotmail.com>
2018-11-01 00:42:18 +0500

    Refactor ReadBenchmarkFile() and add an asymmetric benchmark to benchmark collection.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,19 +13,24 @@ namespace ConsoleApp
     {
         const char EOF = '\uffff';
 
-        static Graph ReadInputLines(string BenchmarkName)
+        static Graph ReadBenchmarkFile(string BenchmarkName)
         {
-            var Lines = File.ReadLines(Directory.GetCurrentDirectory().Replace("\\bin\\Debug", @"\Benchmark Files\" + BenchmarkName));
+            IEnumerable<string> Lines = File.ReadLines(Directory.GetCurrentDirectory().Replace("\\bin\\Debug", @"\Benchmark Files\" + BenchmarkName));
             int Size = 0;
             int DiagonalSize = 0;
             int[] RawInput = null;
             int i = 0;
-            foreach (var Line in Lines)
+
+            // Regex instance to represent 1 or more whitespace occurrences.
+            // Used as a variable here to avoid initialization overhead on every iteration.
+            Regex RegexWhitespaces = new Regex(@"\s+");
+
+            foreach (string Line in Lines)
             {
                 // Skip first seven lines.
                 if (i < 7)
                 {
-                    if (i == 3) // Get Dimensions from the input file.
+                    if (i == 3) // Get no. of vertices/cities from the input file.
                     {
                         Size = Convert.ToInt32(Line.Replace("DIMENSION: ", ""));
                         DiagonalSize = ((int)(Math.Pow(Size, 2) - Size) / 2) + Size;        // (Size^2 - Size / 2) + Size
@@ -39,32 +44,25 @@ namespace ConsoleApp
 
                 else
                 {
-                    Regex RegexWhitespaces = new Regex(@"\s+");
+                    string[] Values = new string[1] { Line };
+                    
+                    // Check if Line has 1 or more occurrences of whitespace characters.
                     if (RegexWhitespaces.Matches(Line).Count > 0)
-                    {
-                        string[] Values = RegexWhitespaces.Replace(Line, " ").Split(' ');
-                        foreach (var Value in Values)
-                        {
-                            if (Value != string.Empty)
-                            {
-                                RawInput[i - 7] = Convert.ToInt32(Value);
-                                ++i;
-                            }
-                        }
-                    }
+                        Values = RegexWhitespaces.Split(Line);                         // Split Line into array of strings upon each occurrence of whitespace characters.
 
-                    else
+                    foreach (string Value in Values)
                     {
-                        RawInput[i - 7] = Convert.ToInt32(Line);
-                        ++i;
+                        if (Value != string.Empty)
+                        {
+                            RawInput[i - 7] = Convert.ToInt32(Value);
+                            ++i;
+                        }
                     }
                 }
             }
 
             Graph Input = new Graph(Size);
-            Console.WriteLine(Input);
 
-            int k = 0;
             /*
             A 3x3 Graph in adjacency matrix form given below,
             *   0    1   2
@@ -77,6 +75,7 @@ namespace ConsoleApp
             0   a   0   b   c   0 
             */
 
+            int k = 0;
             for (i = 0; i < Size; ++i)  // i runs for number of nodes/cities
             {
                 for (int j = 0; j < i + 1; ++j) // j runs until diagonal is reached for each node [inclusive].
@@ -88,7 +87,7 @@ namespace ConsoleApp
 
         static void Main(string[] args)
         {
-            Graph Input = ReadInputLines("Test1.tsp");
+            Graph Input = ReadBenchmarkFile("fri26.tsp");
 
             Console.WriteLine("\n" + Input.ToMatrixString());
             
